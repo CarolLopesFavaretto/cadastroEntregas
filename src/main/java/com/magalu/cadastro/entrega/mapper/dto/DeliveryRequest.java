e7b00d5fe7c0732f4a915dd377aa936d28e0391a
Caroline <carolinelopesrdg@hotmail.com>
2024-11-27 17:44:49 -0300

    refact;

--- src/main/java/com/magalu/cadastro/entrega/mapper/dto/DeliveryRequest.java
+++ src/main/java/com/magalu/cadastro/entrega/mapper/dto/DeliveryRequest.java
@@ -2,10 +2,10 @@ package com.magalu.cadastro.entrega.mapper.dto;
 
 import java.time.LocalDate;
 
-public record DeliveryRequest (Long id,
-                              int quantityPackages,
-                              LocalDate deliveryDeadline,
-                              String nameClient,
-                              String cpfClient,
-                              AddressDTO address) {
+public record DeliveryRequest(
+        int quantityPackages,
+        LocalDate deliveryDeadline,
+        String nameClient,
+        String cpfClient,
+        AddressDTO address) {
 }
